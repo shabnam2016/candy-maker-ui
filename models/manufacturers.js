4bf3936f0e7bbffa01379982b14e91d8ebb3adbd
shabnam <shabnam.mostary@gmail.com>
2021-01-05 18:20:17 -0500

    part one

--- models/manufacturers.js
+++ models/manufacturers.js
@@ -1,9 +1,7 @@
-const Manufacturers = (connection, Sequelize) => {
-  return connection.define('manufacturers', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-    name: { type: Sequelize.STRING, allowNull: false },
-    country: { type: Sequelize.STRING },
-  }, { paranoid: true })
-}
+export default (connection, Sequelize) => connection.define('manufacturers', {
+  id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+  name: { type: Sequelize.STRING, allowNull: false },
+  country: { type: Sequelize.STRING },
+}, { paranoid: true })
 
-module.exports = Manufacturers
+
